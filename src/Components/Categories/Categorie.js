9c9057e04acb12902e55bc2106dda83b467c866c
abdourifki <abdourifki1995@gmail.com>
2023-12-19 14:11:39 +0100

    second commit

---
+++ src/Components/Categories/Categorie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Categories/Categorie.css'
+const Categorie = () => {
+    return (
+        <div>
+            this is categories page
+        </div>
+    );
+}
+
+export default Categorie;
